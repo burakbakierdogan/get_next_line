2faf0cc53c255de20cddede15e7f0383abf9a43b
Burak baki Erdogan <berdogan@k2m04s01.42kocaeli.com.tr>
2022-06-06 15:54:13 +0300

    define

--- get_next_line.c
+++ get_next_line.c
@@ -6,18 +6,18 @@
 /*   By: berdogan <berdogan@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 14:28:57 by berdogan          #+#    #+#             */
-/*   Updated: 2022/05/03 14:28:57 by berdogan         ###   ########.fr       */
+/*   Updated: 2022/06/06 15:51:55 by berdogan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
+#define BUFFER_SIZE
 char	*get_next_line(int fd)
 {
 	char *str;
 	int j =0;
-
-	read(fd,str,1);
+	str = (char *) malloc (20);
+	read(fd,str,BUFFER_SIZE);
 	j++;
 	str[1] = '\0';
 	return (str);
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: berdogan <berdogan@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 14:29:04 by berdogan          #+#    #+#             */
-/*   Updated: 2022/05/03 14:29:04 by berdogan         ###   ########.fr       */
+/*   Updated: 2022/06/06 15:52:46 by berdogan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 
+#ifndef BUFFER_SIZE
+#	define BUFFER_SIZE 10
+#endif
 char	*get_next_line(int fd);
 int	ft_fd(char *str, char *mod);
