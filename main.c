5370a84fbda8ae929f4636f55dbb01e5de0b3f6c
burak baki erdogan <bbe2007@outlook.com>
2022-06-12 17:49:32 +0300

    deneme

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: berdogan <berdogan@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 16:26:40 by berdogan          #+#    #+#             */
-/*   Updated: 2022/06/12 17:46:15 by berdogan         ###   ########.fr       */
+/*   Updated: 2022/06/12 17:48:05 by berdogan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,4 @@ int main ()
 		printf("%s", str);
 	}
 }
-dnemememememe
+
